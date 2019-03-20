2806f5544c2fedb539ca1530702822c457379900
enoler <enolin7@gmail.com>
2019-03-20 14:22:50 +0100

    Items are shown and removed from the list

--- app/src/main/java/es/enoler/consumer_producer/activities/MainActivity.java
+++ app/src/main/java/es/enoler/consumer_producer/activities/MainActivity.java
@@ -1,13 +1,15 @@
 package es.enoler.consumer_producer.activities;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import es.enoler.consumer_producer.R;
+import es.enoler.consumer_producer.adapters.ItemsAdapter;
 import es.enoler.consumer_producer.models.Consumer;
 import es.enoler.consumer_producer.models.Item;
 import es.enoler.consumer_producer.models.Person;
@@ -17,10 +19,11 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-	private Context mContext;
 	private ArrayList<Person> mConsumers;
 	private ArrayList<Person> mProducers;
 	private static ArrayList<Item> mItems;
+	private static ItemsAdapter mItemsAdapter;
+	private static int mItemId;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +32,17 @@ public class MainActivity extends AppCompatActivity {
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		mContext = getApplicationContext();
 		initData();
 		initUI();
 	}
 
 	public static void createItem(int idProducer) {
 		if(mItems != null) {
-			int id = mItems.size() + 1;
-			String name = "Item " + id;
-			Item item = new Item(id, name, idProducer);
+			mItemId++;
+			String name = "Item " + mItemId;
+			Item item = new Item(mItemId, name, idProducer);
 			mItems.add(item);
+			mItemsAdapter.notifyDataSetChanged();
 			Log.d("Test", item.getName());
 		}
 	}
@@ -48,11 +51,19 @@ public class MainActivity extends AppCompatActivity {
 		mConsumers = new ArrayList<>();
 		mProducers = new ArrayList<>();
 		mItems = new ArrayList<>();
+		mItemsAdapter = new ItemsAdapter(this, mItems);
+		if(mItems.size() == 0) mItemId = 0;
+		else mItemId = mItems.get(mItems.size() - 1).getId();
 	}
 
 	private void initUI() {
 		Button btConsumer = findViewById(R.id.bt_add_consumer);
 		Button btProducer = findViewById(R.id.bt_add_producer);
+		RecyclerView rvItems = findViewById(R.id.rv_items);
+		rvItems.setLayoutManager(new LinearLayoutManager(this));
+		rvItems.setHasFixedSize(true);
+
+		rvItems.setAdapter(mItemsAdapter);
 
 		btProducer.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -73,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
 	public static void removeItem() {
 		if(mItems != null && mItems.size() > 0) {
 			mItems.remove(0);
+			mItemsAdapter.notifyDataSetChanged();
 		}
 		Log.d("Test", "Items: " + mItems.size());
 	}
